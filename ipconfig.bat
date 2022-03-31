294c00c90d9d5236b5e709841c2512c502a734ae
kekko code <102719668+Kekko0code@users.noreply.github.com>
2022-03-30 19:47:46 -1200

    Add files via upload
    rubare le specifiche  di rete con un solo clik

---
+++ ipconfig.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "\specifiche\uscita"
+rem in cd metti la directory dove deve venire salvato il file 
+ipconfig /all  >>   ip.txt
+rem in ip.txt mettere il nome con cui vuoi salavre il file 
